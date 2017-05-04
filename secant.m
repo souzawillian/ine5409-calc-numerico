ab84c9afc114f7caeb6a70740659b2d6d65a087e
Willian de Souza <willianstosouza@gmail.com>
2017-05-04 14:54:09 -0300

    Plot secant line at function

--- secant.m
+++ secant.m
@@ -1,10 +1,21 @@
 function secant(x0, x1)
   e = 1E-6;
   count = 0;
-  
+  axis_x = [x0:0.1:x1];
+
+  plot(axis_x, f(axis_x))
+  grid
+  hold on
+
   do
     fx0 = f(x0);
     fx1 = f(x1);
+    r = fx0 + ((fx1-fx0) / (x1-x0)) * (axis_x - x0);
+
+    plot(axis_x, r, 'r')
+    hold on
+    pause
+
     x2 = x1 - (((x1 - x0) * fx1) / (fx1 - fx0));
     x0 = x1;
     x1 = x2;
